c1b35994074c1ec1f1a206a57113b218a06b4b38
themoonisred <samwise47@gmail.com>
2022-11-05 17:20:10 -0700

    I am the captain now

--- Ben/app.js
+++ Ben/app.js
@@ -4,3 +4,4 @@ console.log("PRESS SAVE LUL")
 console.log("I am a woman's rights. I have plowed and reaped and husked and chopped and mowed, and can any man do more than that? I am as strong as any man that is now'A former slave and one of the most powerful orators of her time, Sojourner Truth fought for the equal rights of Black women throughout her life")
 console.log("Sweet are the uses of adversity which, like the toad, ugly and venomous, wears yet a precious jewel in his head")
 vince is WebGLVertexArrayObject
+console.log("THis is my folder now - Sam")
