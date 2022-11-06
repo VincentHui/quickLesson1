14594ff0567c865a0f107a9f911712788e4e794c
benballerboi <xcbencx@gmail.com>
2022-11-05 17:16:09 -0700

    lesgo

---
+++ Ben/app.js
@@ -0,0 +1,6 @@
+console.log("I AM THE BEST")
+console.log("VINCE IS GAY")
+console.log("PRESS SAVE LUL")
+console.log("I am a woman's rights. I have plowed and reaped and husked and chopped and mowed, and can any man do more than that? I am as strong as any man that is now'A former slave and one of the most powerful orators of her time, Sojourner Truth fought for the equal rights of Black women throughout her life")
+console.log("Sweet are the uses of adversity which, like the toad, ugly and venomous, wears yet a precious jewel in his head")
+vince is WebGLVertexArrayObject
