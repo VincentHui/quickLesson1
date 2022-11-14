059b9e85e5daff36e8dc6eae51e858cfae2e83c8
Vincent Hui <vincent.hui@axi.com>
2022-11-14 16:56:09 +0000

    adding simple function definitons

--- vince/app.js
+++ vince/app.js
@@ -6,3 +6,20 @@ console.log("im gay");
 question("what do you think of vince? ", (userInput) =>
   console.log("this is what you think: " + userInput)
 );
+
+//these comments are just ways to annotate code it is not picked up by the compiler
+function MyFunctionName() {
+  //put logic in here in the scope of these curly brackets
+}
+
+function MyFunctionThatReturnsString() {
+  return "blah";
+}
+
+function MyFunctionThatTakesParameter(parameter1) {
+  console.log(parameter1);
+}
+
+function functionThatTakesParameterAndReturns(A, B) {
+  return A + B;
+}
