d6dad6562da6dfb2b5054d7342d9a448290c7345
themoonisred <samwise47@gmail.com>
2022-11-25 14:48:30 -0800

    New If Question statement

--- Sam/app.js
+++ Sam/app.js
@@ -3,6 +3,22 @@ const { question } = require("../vince/question.js");
 console.log("I am Sam");
 console.log("Shutup Vince");
 
-question("what do you think of ben? ", (inputFromConsole) => {
-  console.log("you think ben is " + inputFromConsole);
-});
+question("what do you think of Captain Sam Hui? ", (input) => {
+  if(input != null)
+    console.log("Captain Sam Hui is " + input);
+  if(input == 0)
+    console.log("Answer the question please")}
+);
+// question("what do you think of Captain Sam Hui",(input) => 
+//{if(input => 0){
+//  console.log("Captain Sam Hui is " + input)}
+//else 
+//{console.log("Answer the question please")}
+//});
+
+//question("What do you think of Captain Sam Hui",(input) => {
+//  if(input.length == 0)
+//     console.log("Answer the question please");
+//   if(input.length > 0)
+//     console.log("Sam Hui is " + input);
+// })
